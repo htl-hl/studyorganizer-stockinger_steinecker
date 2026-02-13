56c61411f863a75c19665a0405107d0fb7376721
Jonathan S <yoyomail456@gmail.com>
2026-02-13 09:13:18 +0100

    Subject Controller $ CRUD

---
+++ views/subject/_form.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Subject $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="subject-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'subjectName')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
