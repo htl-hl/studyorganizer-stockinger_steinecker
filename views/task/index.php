9c6ca6e3556fe4d2f9a937a77c214e5971562496
Konrad <konrad.stockinger@it.htl-hl.ac.at>
2026-02-19 14:53:37 +0100

    Added the card design to the task

--- views/task/index.php
+++ views/task/index.php
@@ -59,7 +59,10 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-3 p-1">
                 <div class="card">
                     <div class="card-header">
-                        <h5><?= $task->taskTitle ?></h5>
+                        <h5><?= $task->taskTitle ?><button class="btn float-end"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                    <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+                                </svg></button></h5>
                         <h6 class="card-subtitle mb-2 text-muted"><?= $task->taskSubject->subjectName ?> | bis <?= $dateFormatted ?></h6>
                     </div>
                     <div class="card-body">
