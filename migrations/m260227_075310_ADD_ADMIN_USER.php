b30e259f38d648f250c7602798f1c93853fd171d
Konrad <konrad.stockinger@it.htl-hl.ac.at>
2026-02-27 08:57:31 +0100

    added admin User

---
+++ migrations/m260227_075310_ADD_ADMIN_USER.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+class m260227_075310_ADD_ADMIN_USER extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%USER}}', [
+            'username' => 'admin',
+            'password' => Yii::$app->getSecurity()->generatePasswordHash('admin'),
+            'authKey' => Yii::$app->getSecurity()->generateRandomString(),
+            'accessToken' => Yii::$app->getSecurity()->generateRandomString(),
+            'role' => 'Admin',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m260227_075310_ADD_ADMIN_USER cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m260227_075310_ADD_ADMIN_USER cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
