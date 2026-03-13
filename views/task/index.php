015ada1d0a7ac8cb79260c70718212ebfd814b22
Konrad <konrad.stockinger@it.htl-hl.ac.at>
2026-03-13 09:02:04 +0100

    Added so there is a done tasks

--- views/task/index.php
+++ views/task/index.php
@@ -21,8 +21,19 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a(Yii::t('app', 'Create Task'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+    <?php
+        $openTasks = [];
+        $doneTasks = [];
+        foreach ($tasks as $task) {
+            if ((bool)$task->isDone) {
+                $doneTasks[] = $task;
+            } else {
+                $openTasks[] = $task;
+            }
+        }
+    ?>
     <div class="row">
-        <?php foreach ($tasks as $task): ?>
+        <?php foreach ($openTasks as $task): ?>
 
             <?php
                 $time = strtotime($task->taskDueDate);
@@ -83,7 +94,73 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
             </div>
+        <?php endforeach; ?>
+    </div>
 
+    <hr>
+    <h1>Done Tasks</h1>
+    <div class="row">
+        <?php foreach ($doneTasks as $task): ?>
+
+            <?php
+                $time = strtotime($task->taskDueDate);
+                $date_formatted = date('d.m.Y',$time);
+                $now = time();
+                $remaining = max($time - $now, 0);
+                $remaining_days = $remaining / 86400;
+                $is_done = (bool)$task->isDone;
+
+                $colours = array();
+                $colours[14] = "primary";
+                $colours[7] = "warning";
+                $colours[1] = "danger";
+
+                $colour = "secondary";
+
+                if (!$is_done) {
+                    foreach ($colours as $threshold => $colour_option) {
+                        if ($threshold >= $remaining_days) {
+                            $colour = $colour_option;
+                        }
+                    }
+                } else {
+                    $colour = "success";
+                }
+            ?>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-2">
+                <div class="card h-100<?= $is_done ? ' border-success' : '' ?>">
+                    <div class="card-header bg-<?= $colour ?>"></div>
+                    <div class="card-header">
+                        <h5> <?= Html::a($task->taskTitle, ['task/view', 'taskId' => $task->taskId], ['class' => 'text-decoration-none']) ?>
+                            <?php if ($is_done): ?>
+                                <span class="btn float-end text-success"><?= Task::doneIcon() ?></span>
+                            <?php else: ?>
+                                <?= \yii\bootstrap5\Html::a(Task::doneIcon(),
+                                        Url::to(["task/done", "taskId" => $task->taskId]),
+                                        ["class" => "btn float-end",
+                                                "data-method" => "POST",
+                                                "data-confirm" => "Mark this task as done?"])
+                                ?>
+                            <?php endif; ?>
+                            <?php if ($is_done): ?>
+                                <span class="btn float-end text-muted"><?= Task::updateIcon() ?></span>
+                            <?php else: ?>
+                                <?= \yii\bootstrap5\Html::a(Task::updateIcon(),
+                                        Url::to(["task/update", "taskId" => $task->taskId]),
+                                        ["class" => "btn float-end"])
+                                ?>
+                            <?php endif; ?>
+                            <button class="btn float-end"></button><button class="btn float-end"></button></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?= $task->taskSubject->subjectName ?> | bis <?= $date_formatted ?></h6>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text text-truncate">
+                            <?= $task->taskDescription ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
     </div>
 
